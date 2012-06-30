870701087f15f2f23b6d7fa1b17c2d1ee4e8facd
Sam Richard <snugug@gmail.com>
2012-06-30 15:16:29 -0400

    RWD Kickstart to Toolkit

--- compass/lib/toolkit.rb
+++ compass/lib/toolkit.rb
@@ -3,4 +3,4 @@ require 'susy'
 require 'respond-to'
 
 extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
-Compass::Frameworks.register('rwd-kickstart', :path => extension_path)
+Compass::Frameworks.register('toolkit', :path => extension_path)
