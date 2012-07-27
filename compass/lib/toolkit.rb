8d99c5271b65bf78cf7922eae4659081bb089fab
Sam Richard <snugug@gmail.com>
2012-07-27 08:18:45 -0400

    Require Singularity

--- compass/lib/toolkit.rb
+++ compass/lib/toolkit.rb
@@ -1,6 +1,14 @@
 require 'compass'
 require 'susy'
 require 'respond-to'
+require 'singularitygs'
 
 extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('toolkit', :path => extension_path)
+
+module Sass::Script::Functions
+  def str_replace(needle, replace, haystack)
+    result = haystack.value.gsub(needle.value, replace.value)
+    Sass::Script::String.new(result)
+  end
+end
