c7daa3c11836a2b4dd078b6b304c5a590f26f775
Scott Kellum <scott@scottkellum.com>
2013-01-07 14:38:32 -0500

    add sass script to test project

--- config.rb
+++ config.rb
@@ -26,3 +26,17 @@ line_comments = false
 # preferred_syntax = :sass
 # and then run:
 # sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+module Sass::Script::Functions
+  def str_replace(needle, replace, haystack)
+    result = haystack.value.gsub(needle.value, replace.value)
+    Sass::Script::String.new(result)
+  end
+
+  def children_of_ie_nth(input)
+    n = "n"
+    b = ""
+    result = input.value.gsub(n, b)
+    Sass::Script::Number.new(result.to_i)
+  end
+end
