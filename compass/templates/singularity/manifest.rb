c1569dbe034b8ef9492fcc8383be7d41d1b0f02f
Sam Richard <snugug@gmail.com>
2013-04-11 22:37:37 +0100

    Updated Manifest files

--- compass/templates/singularity/manifest.rb
+++ compass/templates/singularity/manifest.rb
@@ -20,12 +20,7 @@ file '../shared/_ie-design.scss', :like => :stylesheet, :to => 'partials/design/
 file '../shared/_print-design.scss', :like => :stylesheet, :to => 'partials/design/_print-design.scss'
 
 file '../shared/print.scss',  :like => :stylesheet, :media => 'print', :to => 'print.scss'
-file '../shared/ie.scss',     :like => :stylesheet, :media => 'screen, projection', :condition => "lt IE 8", :to => 'ie.scss'
-
-# JavaScript
-file '../shared/hammer.js',           :like => :javascript, :to => 'hammer.js'
-file '../shared/loader.js',           :like => :javascript, :to => 'loader.js'
-file '../shared/modernizr-2.5.3.js',  :like => :javascript, :to => 'modernizr-2.5.3.js'
+file '../shared/ie.scss',     :like => :stylesheet, :media => 'screen, projection', :condition => "lte IE 8", :to => 'ie.scss'
 
 help %Q{
   Please contact Sam Richard or Mason Wendell with questions:
@@ -37,34 +32,29 @@ help %Q{
 welcome_message %Q{
 
   Responsive Web Design Kickstart
-  
+
   Welcome to the Responsive Web Design Kickstart gem! Provided are the basic tools needed to kickstart a custom Responsive Web Design solution tailor made to your needs! Remember, tools and best practices are always changing, so keep up to date!
-  
+
 The following is included in this build:
   Compass Extensions:
    * Singularity
    * Toolkit
-   
-  JavaScript:
-   * Modernizr Custom Build with all Standard features of 2.3.5 except html5shiv w/printshiv or Media Queries. Build your own from http://modernizr.com/download/ when you are ready for production
-   * Loader.js custom JavaScript file for holding conditional asset loading scripts thanks to Modernizr's yepnope (http://yepnopejs.com/). Comes with yepnope code for loading in Hammer.js, just make sure the path is correct from root.. Ready for production, but remember to minify and gzip!
-   * Hammer.js (http://eightmedia.github.com/hammer.js/) for awesome touch events. Ready for production, but remember to minify and gzip!
-    
+
   Include the following in your <head> tag:
-  
+
   <meta content='width=device-width, initial-scale=1.0' name='viewport' />
   <meta content='IE=edge,chrome=1' http-equiv='X-UA-Compatible' />
   <!--[if lt IE 9 ]> <script src="//ajax.googleapis.com/ajax/libs/chrome-frame/1.0.2/CFInstall.min.js"></script><script>window.attachEvent("onload",function(){CFInstall.check({mode:"overlay"})})</script> <![endif]-->
   <!-- CSS -->
   <link href='path/to/css/style.css?v=1' rel='stylesheet' />
-  <!-- All JavaScript at the bottom, except for Modernizr and Respond/Selectivizr for IE 8 and Below. -->
-  <script src='path/to/js/modernizr-2.5.3.js'></script>
-  
+  <!-- All JavaScript at the bottom, except for Modernizr. -->
+
+
   Include the following, and all other JavaScript, including any JavaScript frameworks you may be using like jQuery, right before your closing </body> tag:
-  
+
   <!-- Javascript at the bottom for fast page loading -->
-  <script src='path/to/js/loader.js'></script>
-  
-  
+
+
+
 }
 
