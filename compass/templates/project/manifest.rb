a788c03c56940671a3e338f556b3821305f8930e
Sam Richard <snugug@gmail.com>
2013-04-11 22:54:01 +0100

    Included the other Sass files in basic Toolkit

--- compass/templates/project/manifest.rb
+++ compass/templates/project/manifest.rb
@@ -1,26 +1,26 @@
-description "Responsive Web Design Kickstart"
+description "Toolkit Kickstart"
 
 # Stylesheets
-# file '../shared/style.scss',  :like => :stylesheet, :media => 'screen, projection', :to => 'style.scss'
+file '../shared/style.scss',  :like => :stylesheet, :media => 'screen, projection', :to => 'style.scss'
 
 file '_base.scss',  :like => :stylesheet, :to => 'partials/global/_base.scss'
-# file '../shared/_variables.scss', :like => :stylesheet, :to => 'partials/global/_variables.scss'
-# file '../shared/_functions.scss', :like => :stylesheet, :to => 'partials/global/_functions.scss'
-# file '../shared/_mixins.scss', :like => :stylesheet, :to => 'partials/global/_mixins.scss'
-# file '../shared/_extendables.scss', :like => :stylesheet, :to => 'partials/global/_extendables.scss'
-#
-# file '../shared/_style-guide.scss', :like => :stylesheet, :to => 'partials/styleguide/_style-guide.scss'
-#
-# file '../shared/_layout.scss', :like => :stylesheet, :to => 'partials/layout/_layout.scss'
-# file '../shared/_ie-layout.scss', :like => :stylesheet, :to => 'partials/layout/_ie-layout.scss'
-# file '../shared/_print-layout.scss', :like => :stylesheet, :to => 'partials/layout/_print-layout.scss'
-#
-# file '../shared/_design.scss', :like => :stylesheet, :to => 'partials/design/_design.scss'
-# file '../shared/_ie-design.scss', :like => :stylesheet, :to => 'partials/design/_ie-design.scss'
-# file '../shared/_print-design.scss', :like => :stylesheet, :to => 'partials/design/_print-design.scss'
-#
-# file '../shared/print.scss',  :like => :stylesheet, :media => 'print', :to => 'print.scss'
-# file '../shared/ie.scss',     :like => :stylesheet, :media => 'screen, projection', :condition => "lt IE 8", :to => 'ie.scss'
+file '../shared/_variables.scss', :like => :stylesheet, :to => 'partials/global/_variables.scss'
+file '../shared/_functions.scss', :like => :stylesheet, :to => 'partials/global/_functions.scss'
+file '../shared/_mixins.scss', :like => :stylesheet, :to => 'partials/global/_mixins.scss'
+file '../shared/_extendables.scss', :like => :stylesheet, :to => 'partials/global/_extendables.scss'
+
+file '../shared/_style-guide.scss', :like => :stylesheet, :to => 'partials/styleguide/_style-guide.scss'
+
+file '../shared/_layout.scss', :like => :stylesheet, :to => 'partials/layout/_layout.scss'
+file '../shared/_ie-layout.scss', :like => :stylesheet, :to => 'partials/layout/_ie-layout.scss'
+file '../shared/_print-layout.scss', :like => :stylesheet, :to => 'partials/layout/_print-layout.scss'
+
+file '../shared/_design.scss', :like => :stylesheet, :to => 'partials/design/_design.scss'
+file '../shared/_ie-design.scss', :like => :stylesheet, :to => 'partials/design/_ie-design.scss'
+file '../shared/_print-design.scss', :like => :stylesheet, :to => 'partials/design/_print-design.scss'
+
+file '../shared/print.scss',  :like => :stylesheet, :media => 'print', :to => 'print.scss'
+file '../shared/ie.scss',     :like => :stylesheet, :media => 'screen, projection', :condition => "lte IE 8", :to => 'ie.scss'
 
 help %Q{
   Please contact Sam Richard or Mason Wendell with questions:
@@ -31,9 +31,9 @@ help %Q{
 
 welcome_message %Q{
 
-  Responsive Web Design Kickstart
+  Toolkit Kickstart
 
-  Welcome to the Responsive Web Design Kickstart gem! Provided are the basic tools needed to kickstart a custom Responsive Web Design solution tailor made to your needs! Remember, tools and best practices are always changing, so keep up to date!
+  Welcome to Toolkit! Provided are the basic tools needed to kickstart a new Sass project tailor made to your needs! Remember, tools and best practices are always changing, so keep up to date!
 
 The following is included in this build:
   Compass Extensions:
