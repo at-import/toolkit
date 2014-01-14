835e1ef03dde8fbae1886d3cecd476c473c89f48
Sam Richard <snugug@gmail.com>
2014-01-14 18:18:12 -0500

    Removed lots of ruby dependencies

--- compass/lib/toolkit.rb
+++ compass/lib/toolkit.rb
@@ -1,16 +1,4 @@
 require 'compass'
-require 'singularitygs'
-require 'sassy-strings'
-require 'color-schemer'
 
 extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 Compass::Frameworks.register('toolkit', :path => extension_path)
-
-module Sass::Script::Functions
-  def children_of_ie_nth(input)
-    n = "n"
-    b = ""
-    result = input.value.gsub(n, b)
-    Sass::Script::Number.new(result.to_i)
-  end
-end
