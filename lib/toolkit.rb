c4a704dc265598b8d96fcb8a32cf142b06da0bd8
Sam Richard <snugug@gmail.com>
2014-07-28 10:34:42 -0400

    Fixed errant path issue

--- lib/toolkit.rb
+++ lib/toolkit.rb
@@ -1,16 +1,17 @@
-toolkit_path = File.expand_path('../stylesheets', __FILE__)
+toolkit_sass_path = File.expand_path('../stylesheets', __FILE__)
+toolkit_path = "#{File.dirname(__FILE__)}/.."
 
 if (defined? Compass)
   Compass::Frameworks.register(
     "toolkit",
-    :stylesheets_directory => toolkit_path
+    :path => toolkit_path
   )
 else
-  ENV["SASS_PATH"] = [ENV["SASS_PATH"], toolkit_path].compact.join(File::PATH_SEPARATOR)
+  ENV["SASS_PATH"] = [ENV["SASS_PATH"], toolkit_sass_path].compact.join(File::PATH_SEPARATOR)
 end
 
 
 module Toolkit
-  VERSION = "2.5.1"
+  VERSION = "2.5.2"
   DATE = "2014-07-24"
 end
