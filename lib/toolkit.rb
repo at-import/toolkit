4eb64643433760e9916243553cdc7f7a952660e1
Sam Richard <snugug@gmail.com>
2014-08-19 14:56:13 -0500

    Updated Sass to work correctly

--- lib/toolkit.rb
+++ lib/toolkit.rb
@@ -1,17 +1,17 @@
-toolkit_sass_path = File.expand_path('../stylesheets', __FILE__)
-toolkit_path = "#{File.dirname(__FILE__)}/.."
+base_directory  = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+toolkit_stylesheets_path = File.join(base_directory, 'stylesheets')
 
 if (defined? Compass)
   Compass::Frameworks.register(
     "toolkit",
-    :path => toolkit_path
+    :path => base_directory
   )
 else
-  ENV["SASS_PATH"] = [ENV["SASS_PATH"], toolkit_sass_path].compact.join(File::PATH_SEPARATOR)
+  ENV["SASS_PATH"] = [ENV["SASS_PATH"], toolkit_stylesheets_path].compact.join(File::PATH_SEPARATOR)
 end
 
 
 module Toolkit
-  VERSION = "2.5.2"
-  DATE = "2014-07-24"
+  VERSION = "2.6.0"
+  DATE = "2014-08-19"
 end
